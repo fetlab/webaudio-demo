8ed8e7faebba24cd41c29c566594dbe487e12ffb
Daniel Ashbrook <dan@danielashbrook.com>
2020-05-04 10:56:11 +0200

    remove irrelevant bits

--- scripts/app.js
+++ scripts/app.js
@@ -1,107 +1,15 @@
 // set up basic variables for app
-
-const record = document.querySelector('.record');
-const stop = document.querySelector('.stop');
-const soundClips = document.querySelector('.sound-clips');
 const canvas = document.querySelector('.visualizer');
-const mainSection = document.querySelector('.main-controls');
-
-// disable stop button while not recording
-
-stop.disabled = true;
-
-// visualiser setup - create web audio api context and canvas
-
-let audioCtx;
 const canvasCtx = canvas.getContext("2d");
+let   audioCtx;
 
 //main block for doing the audio recording
-
-if (navigator.mediaDevices.getUserMedia) {
-  console.log('getUserMedia supported.');
-
+if(navigator.mediaDevices.getUserMedia) {
   const constraints = { audio: true };
-  let chunks = [];
 
   let onSuccess = function(stream) {
     const mediaRecorder = new MediaRecorder(stream);
-
     visualize(stream);
-
-    record.onclick = function() {
-      mediaRecorder.start();
-      console.log(mediaRecorder.state);
-      console.log("recorder started");
-      record.style.background = "red";
-
-      stop.disabled = false;
-      record.disabled = true;
-    }
-
-    stop.onclick = function() {
-      mediaRecorder.stop();
-      console.log(mediaRecorder.state);
-      console.log("recorder stopped");
-      record.style.background = "";
-      record.style.color = "";
-      // mediaRecorder.requestData();
-
-      stop.disabled = true;
-      record.disabled = false;
-    }
-
-    mediaRecorder.onstop = function(e) {
-      console.log("data available after MediaRecorder.stop() called.");
-
-      const clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
-
-      const clipContainer = document.createElement('article');
-      const clipLabel = document.createElement('p');
-      const audio = document.createElement('audio');
-      const deleteButton = document.createElement('button');
-
-      clipContainer.classList.add('clip');
-      audio.setAttribute('controls', '');
-      deleteButton.textContent = 'Delete';
-      deleteButton.className = 'delete';
-
-      if(clipName === null) {
-        clipLabel.textContent = 'My unnamed clip';
-      } else {
-        clipLabel.textContent = clipName;
-      }
-
-      clipContainer.appendChild(audio);
-      clipContainer.appendChild(clipLabel);
-      clipContainer.appendChild(deleteButton);
-      soundClips.appendChild(clipContainer);
-
-      audio.controls = true;
-      const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
-      chunks = [];
-      const audioURL = window.URL.createObjectURL(blob);
-      audio.src = audioURL;
-      console.log("recorder stopped");
-
-      deleteButton.onclick = function(e) {
-        let evtTgt = e.target;
-        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-      }
-
-      clipLabel.onclick = function() {
-        const existingName = clipLabel.textContent;
-        const newClipName = prompt('Enter a new name for your sound clip?');
-        if(newClipName === null) {
-          clipLabel.textContent = existingName;
-        } else {
-          clipLabel.textContent = newClipName;
-        }
-      }
-    }
-
-    mediaRecorder.ondataavailable = function(e) {
-      chunks.push(e.data);
-    }
   }
 
   let onError = function(err) {
@@ -127,32 +35,35 @@ function visualize(stream) {
   const dataArray = new Uint8Array(bufferLength);
 
   source.connect(analyser);
-  //analyser.connect(audioCtx.destination);
 
-  draw()
+  draw();
 
   function draw() {
-    const WIDTH = canvas.width
+    const WIDTH = canvas.width;
     const HEIGHT = canvas.height;
 
+		//Run draw() about 60 times/second -> we put it here so it gets
+		// called again the next time
     requestAnimationFrame(draw);
 
-    analyser.getByteTimeDomainData(dataArray);
+    //analyser.getByteFrequencyData(dataArray);   //Get FFT
+    analyser.getByteTimeDomainData(dataArray);    //Get waveform
 
+		//Clear the canvas
     canvasCtx.fillStyle = 'rgb(200, 200, 200)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    canvasCtx.lineWidth = 2;
+		//Set up visualization line
+    canvasCtx.lineWidth = 1;
     canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
 
+		//Everything from here down is drawing
     canvasCtx.beginPath();
 
     let sliceWidth = WIDTH * 1.0 / bufferLength;
     let x = 0;
 
-
     for(let i = 0; i < bufferLength; i++) {
-
       let v = dataArray[i] / 128.0;
       let y = v * HEIGHT/2;
 
@@ -172,6 +83,7 @@ function visualize(stream) {
 }
 
 window.onresize = function() {
+	const mainSection = document.querySelector('.main-controls');
   canvas.width = mainSection.offsetWidth;
 }
 
